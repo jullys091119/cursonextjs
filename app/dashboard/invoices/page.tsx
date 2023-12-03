202ab9543e7d702290fdc8209369b3e0f83ba7f0
julian ontiveros <informacionlinux271087@gmail.com>
2023-12-03 13:19:43 -0700

    primer comit

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,3 @@
+export default function invoicespage() {
+    return <p>Pagina Invoices</p>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { monserrat } from './ui/fonts';
+import './ui/global.css';
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      
+      <body className={`${monserrat.className} antialised`}>
+        <h1>HOla mundo</h1>
+        {children}
+        <footer className='py-10 flex justify-center item-center'>
+          Hecho con amor por la gente de vercel
+        </footer>
+      </body>
     </html>
   );
 }
